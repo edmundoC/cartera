f92619f0794b2220eff1b8af1bf3db32dad31d00
edmundoC <lokopro90@gmail.com>
2015-04-21 01:36:17 -0500

    Actualización, muestra las promesas de cada cliente con la ayuda de jQuery Ajax

--- index.php
+++ index.php
@@ -1,28 +1,36 @@
- <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> 
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 	<meta charset="UTF-8">
 	<title>Cartera de clientes</title>
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 	<script type="text/javascript" src="scripts/jquery-2.1.3.min.js"></script>
 	<script type="text/javascript">
 		$(document).on("ready",function(){
-			$('#btnActualizar').on("click",function(){
-				
-			});
-			$.ajax({
-					beforeSend: function(){
+			var mostrando = false;
+	
+			$("tbody tr:odd").css("background","#bbbbff");
 
-					},
-					data: {operacion:"c"},
-					type: "POST",
-					url: "operaciones.php",
-					success: function(respuesta){
-						$('#datos').html(respuesta);
-					},
-					error: function(a,b,c){
-						alert("Valor de a: "+a+" Valor de B: "+b+" Valor de C: "+c);
-					}
-			});	
+			$("tbody tr").on("click",function(){
+				if(mostrando){
+					$("#cont-promesas").remove();
+					mostrando = false;
+				}else{
+					var elementoActual = $(this);
+					$.ajax({
+						url:'operaciones.php',
+						method:'POST',
+						data:{operacion:'p',id:$(this).attr('id')},
+						success: function(respuesta){
+							var info = $('<div id="cont-promesas"></div>');
+							$(elementoActual).after(info);
+							$("#cont-promesas").html(respuesta);
+							$("#tabla-promesas tbody tr:even").css("background","#bbffbb");
+						},
+					});
+				mostrando = true;
+				}
+			});
 		});
 	</script>
 </head>
@@ -33,13 +41,36 @@
 		if(isset($_SESSION['usuario'])){
 			?><h2>Bienvenido: <?echo $_SESSION['usuario'] ?> a tu cartera de clientes</h2>
 			<div id=datos class="cont-clientes">
+				<table id="tabla_clientes" border="1" style="a">
+					<thead><tr>
+						<th>Nombre</th>
+						<th>Estado</th>
+					</tr></thead>	
+					<?php
+						$id_usuario = $_SESSION['id_usuario'];
+						$query = "SELECT concat(c.nombre,' ',c.apellidos) AS cliente, e.nombre estado from clientes c, estados e WHERE id_usuario = '".$id_usuario."' AND c.id_estado = e.id_estado";
+						$result = mysql_query($query,$conexion) or die ("Error en la consulta: ".mysql_error());
+						$contador = 1;
+						while($datos = mysql_fetch_row($result)){
+							echo '<tr id="'.$contador.'">';
+							echo "<td>".$datos[0]."</td>";
+							echo "<td>".$datos[1]."</td>";
+							echo "</tr>";
+
+							$contador++;
+						}
+					?>
+					<tfoot>
+						<tr>
+							<td colspan="2" align="center">
+								<input id="btnAgregar" type="button" value="Agregar"/>
+								<input id="btnModificar" type="button" value="Modificar"/>
+								<input id="btnEliminar	" type="button" value="Eliminar"/>
+							</td>
+						</tr>
+					</tfoot>
+				</table>
 			</div>
-			<table>
-				<tr>
-					<td><input id="btnAgregar" type="button" value="Agregar Clientes"/></td>
-					<td><input id="btnModificar" type="button" value="Modificar Clientes"/></td>
-				</tr>
-			</table>
 		<?}
 	?>
 </body>
--- operaciones.php
+++ operaciones.php
@@ -5,22 +5,46 @@
 		$operacion = $_POST['operacion'];
 		$id_usuario = $_SESSION['id_usuario'];
 		switch ($operacion) {
-			case "c":
-				$query = "SELECT concat(c.nombre,' ',c.apellidos) AS cliente, e.nombre from clientes c, estados e WHERE id_usuario = '".$id_usuario."' AND c.id_estado = e.id_estado";
+			case 'c':
+				/*$query = "SELECT concat(c.nombre,' ',c.apellidos) AS cliente, e.nombre from clientes c, estados e WHERE id_usuario = '".$id_usuario."' AND c.id_estado = e.id_estado";
 				$result = mysql_query($query) or die("Error en la consulta: ".mysql_error());
-				echo '<table border="1"><tr><th>Cliente</th><th>Estado</th></tr>';
+				
+				$contador = 1;
 				while ($datos = mysql_fetch_row($result)) {
-					echo "<tr>";
+					echo "<tr id='".$contador."'>";
 					echo "<td><center>".$datos[0]."</center></td>";
 					echo "<td><center>".$datos[1]."</center></td>";
+					echo '<td><center><input id="btnModificar" type="button" value="Modificar"/></center></td>';
+					echo '<td><center><input id="btnEliminar" type="button" value="Eliminar"/></center></td>';
 					echo "</tr>";
+					$contador++;
 				}
-				echo "</table>";
+				*/
+				echo "Entro a clientes";
 				break;
-			
+			case 'l':
+				echo "Entro a logout";
+			break;
+			case 'p':
+				$idCliente = $_POST['id'];
+				echo '<table id="tabla-promesas" border=1">';
+				echo '<thead><tr><th>Num Folio</th><th>Fecha de pago</th><th>Monto</th></tr></thead>';
+				$query = "SELECT num_folio, fecha_pago, monto from folios AS f, promesas AS p where f.id_cliente = '".$idCliente."' AND p.id_folio = f.id_folio";	
+				$result = mysql_query($query);
+				if(mysql_num_rows($result) != 0){
+					while ($row = mysql_fetch_row($result)){
+							echo "<tr>";
+    						echo "<td>$row[0]</td>";
+							echo "<td>$row[1]</td>";
+							echo "<td>$row[2]</td>";
+							echo "</tr>";
+					}
+				}
+				echo '</table>';
+			break;
 			default:
-				echo "No entro a mostrar";
-				break;
+				echo "Opcion invalida";
+			break;
 		}
 	}else{
 		echo "Error, algo salio mal";
